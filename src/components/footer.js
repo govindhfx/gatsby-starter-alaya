4a2172da656d3279f951870d76f8353de8859334
grtalluri <talluri@gmail.com>
2021-05-03 10:12:38 -0300

    Footer Updated - Alaya Foods -May 3rd, 2021

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,11 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
+        Alaya Foods Inc. &copy; 2021, Made with{" "}
         <span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        by <Link to="/">IAMGRT.COM</Link>
       </p>
     </div>
   </footer>
